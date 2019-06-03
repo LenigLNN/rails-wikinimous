baa8d2e914c9f6fb5d153575fb4c264b2c24f0fd
Lena Nigrell <nigrell@live.se>
2019-06-03 17:26:22 +0200

    1st version rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Creating 10 fake posts...'
+10.times do
+  article = Article.new(
+    title: Faker::Name.name_with_middle,
+    content: Faker::String.random
+  )
+  article.save!
+end
